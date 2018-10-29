de3f1a94a3b2a592228d09c949c63026f570c4c5
Juan De Leon <dele17822@uvg.edu.gt>
2018-10-28 20:07:48 -0600

    Navigate between stages

--- src/userMenu/UserMenuController.java
+++ src/userMenu/UserMenuController.java
@@ -1,4 +1,119 @@
 package userMenu;
 
 public class UserMenuController {
+
+    public void withdrawalMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../withdrawalMenu/WithdrawalMenuController.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Withdrawal Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void accountBalanceMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../accountBalanceMenu/AccountBalanceMenu.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Account Balance Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void accountMovesMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../accountMoves/movesMenu.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Account moves Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void wireTransferMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../wireTrasfer/trasnfersMenu.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Wire Transfer Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void changePinMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../changePin/changePin.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Change Pin Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void MainMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../sample/sample.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Main Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/withdrawalMenu/WithdrawalMenuController.java
+++ src/withdrawalMenu/WithdrawalMenuController.java
@@ -1,4 +1,43 @@
 package withdrawalMenu;
 
 public class WithdrawalMenuController {
+
+    public void variableAmountMenuWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VariableAmountMenu.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Variable Amount Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void returnWindow(ActionEvent event){
+        Parent root1;
+
+        try {
+            //Cierra la actual ventana
+            ((Node)event.getSource()).getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../userMenu/UserMenu.fxml"));
+            root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("User Menu");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
